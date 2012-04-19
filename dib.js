17452ffb6a0dd9997107c17f297025b471ac3085
Jared Hanson <jaredhanson@gmail.com>
2012-04-19 15:25:46 -0700

    Implement engine registration.

--- dib.js
+++ dib.js
@@ -2,26 +2,52 @@ define(['anchor/dom'],
 
 function(dom) {
   
+  var engines = {};
+  
   function Dib(options) {
     this._id = options.id;
   }
   
-  Dib.prototype.load = function(ctxt) {
+  Dib.engine = function(name, fn) {
+    if (typeof name == 'function') {
+      fn = name;
+      name = 'default';
+    }
+    engines[name] = fn;
+  }
+  
+  Dib.prototype.load = function(ctxt, cb) {
     // TODO: Implement async rendering with callback.
     // TODO: Register rendering functions. Don't assume Hogan.
     // TODO: Figure out how to extract variables for binding layer.
     // TODO: Eliminate jQuery dependency.
-    var templ = Hogan.compile($(this._id).html());
-    var html = templ.render(ctxt);
-    return dom.fromHTML(html);
+    
+    var engine = 'default';
+    
+    fn = engines[engine];
+    if (!fn) { throw new Error("Can't find template engine: " + engine); }
+    
+    fn(this._id, ctxt, function(err, str) {
+      if (err) { return cb(err) }
+      // TODO: May not want to always wrap the string in a DOM node collection.
+      var node = dom.fromHTML(str);
+      cb(err, node);
+    });
   }
   
   Dib.prototype.hook = function(ctrl, el, events) {
     for (var prop in events) {
       // TODO: Parse out event and CSS selector
       //console.log('prop: ' + prop + ' to: ' + events[prop]);
-      var action = events[prop];
-      el.on(prop, ctrl[action]);
+      var fn;
+      if (typeof events[prop] == 'function') {
+        fn = events[prop];
+      } else {
+        // TODO: Should the function be bound to preserve this context?
+        var action = events[prop];
+        fn = ctrl[action];
+      }
+      el.on(prop, fn);
     }
   }
   
