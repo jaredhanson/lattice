b090a343dd8c46abe7c4626c49af7011add3fc0a
Jared Hanson <jaredhanson@gmail.com>
2012-04-20 11:13:53 -0700

    Flesh out event listening.

--- dib.js
+++ dib.js
@@ -9,63 +9,54 @@ function(dom) {
   }
   
   Dib.prototype.create = function(options, cb) {
+    if (typeof options == 'function') {
+      cb = options;
+      options = {};
+    }
+    options = options || {};
+    
     // TODO: Figure out how to extract variables for binding layer.
     // TODO: Eliminate jQuery dependency.
     
-    var engine = 'default';
-    
-    fn = engines[engine];
-    if (!fn) { throw new Error("Can't find template engine: " + engine); }
+    var engine = options.engine || 'default';
+    var render = engines[engine];
+    if (!render) { throw new Error("Can't find template engine: " + engine) }
     
     var self = this;
-    fn(this._name, options, function(err, str) {
+    render(this._name, options, function(err, str) {
       if (err) { return cb(err) }
       // TODO: May not want to always wrap the string in a DOM node collection.
       var el = dom.fromHTML(str);
       self._triggers(el);
       
-      cb(err, el);
+      cb(null, el);
     });
   }
   
   Dib.prototype._triggers = function(el) {
     var events = this._events;
     for (var key in events) {
-      // TODO: Break key out into event and selector
-      console.log('trigger key: ' + key);
-      var action = events[key];
-      var ev = key;
-      var handler;
+      var parts = key.split(' ')
+        , ev = parts[0]
+        , selector = parts[1]
+        , action = events[key]
+        , target = this._target
+        , handler;
+        
+      // TODO: Scope the element to the selector, if one is specified.
       
       if (typeof action == 'function') {
-        console.log('target function');
         handler = action;
       } else if (typeof action == 'string') {
-        console.log('target string');
-        handler = this._target[action];
+        if (!target) { throw new Error("Unspecified target for action: " + action) }
+        handler = target[action].bind(target);
       } else if (typeof action == 'object') {
-        console.log('target object');
+        target = action.target || target;
+        if (!target) { throw new Error("Unspecified target for action: " + action.action) }
+        handler = target[action.action].bind(target);
       }
       
       el.on(ev, handler);
-    }
-  }
-  
-  Dib.prototype.hook = function(ctrl, el, events) {
-    for (var prop in events) {
-      // TODO: Parse out event and CSS selector
-      //console.log('prop: ' + prop + ' to: ' + events[prop]);
-      /*
-      var fn;
-      if (typeof events[prop] == 'function') {
-        fn = events[prop];
-      } else {
-        // TODO: Should the function be bound to preserve this context?
-        var action = events[prop];
-        fn = ctrl[action];
-      }
-      el.on(prop, fn);
-      */
     }
   }
   
